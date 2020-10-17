1586aca4a07d1ba291f8db4a61a6601e24067089
Caio Vieira <caiodmvieiragithub@gmail.com>
2020-10-16 22:12:08 -0300

    refactor: move sut creation to a factory helper method

--- src/presentation/controllers/debits/add-debit-controller.spec.ts
+++ src/presentation/controllers/debits/add-debit-controller.spec.ts
@@ -1,36 +1,52 @@
 import { AddDebitController } from './add-debit-controller'
 import { AddDebitModel, DebitModel, AddDebit } from './add-debit-protocols'
+import { HttpRequest } from '../../protocols'
+
+const makeFakeRequest = (): HttpRequest => ({
+  body: {
+    userId: 'any_id',
+    reason: 'any_reason',
+    date: 'any_date',
+    value: 'any_value'
+  }
+})
+
+const makeFakeDebit = (): DebitModel => ({
+  id: 'any_id',
+  userId: 'any_userid',
+  reason: 'any_reason',
+  date: 'any_date',
+  value: 'any_value'
+})
+
+const makeAddDebit = (): AddDebit => {
+  class AddDebitStub implements AddDebit {
+    async add (debit: AddDebitModel): Promise<DebitModel> {
+      return new Promise(resolve => resolve(makeFakeDebit()))
+    }
+  }
+  return new AddDebitStub()
+}
+interface SutTypes {
+  sut: AddDebitController
+  addDebitStub: AddDebit
+}
+
+const makeSut = (): SutTypes => {
+  const addDebitStub = makeAddDebit()
+  const sut = new AddDebitController(addDebitStub)
+  return {
+    sut,
+    addDebitStub
+  }
+}
 
 describe('AddDebit Controller', () => {
   test('Should AddDebit Controller calls AddDebit with correct values', async () => {
-    class AddDebitStub implements AddDebit {
-      async add (debit: AddDebitModel): Promise<DebitModel> {
-        return new Promise(resolve => resolve({
-          id: 'any_id',
-          userId: 'any_userid',
-          reason: 'any_reason',
-          date: 'any_date',
-          value: 'any_value'
-        }))
-      }
-    }
-    const addDebitStub = new AddDebitStub()
-    const sut = new AddDebitController(addDebitStub)
+    const { sut, addDebitStub } = makeSut()
     const addSpy = jest.spyOn(addDebitStub,'add')
-    await sut.handle({
-      body: {
-        userId: 'any_id',
-        reason: 'any_reason',
-        date: 'any_date',
-        value: 'any_value'
-      }
-    })
+    await sut.handle(makeFakeRequest())
 
-    expect(addSpy).toBeCalledWith({
-      userId: 'any_id',
-      reason: 'any_reason',
-      date: 'any_date',
-      value: 'any_value'
-    })
+    expect(addSpy).toBeCalledWith(makeFakeRequest().body)
   })
 })
