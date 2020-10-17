8aec821c57ddfdaf0f293721d3d82a11dfca5b92
Caio Vieira <caiodmvieiragithub@gmail.com>
2020-10-17 17:43:00 -0300

    test: ensure DbAddDebit calls LoadClientByIdRepository with correct value

--- src/data/usecases/add-debit/db-add-debit.spec.ts
+++ src/data/usecases/add-debit/db-add-debit.spec.ts
@@ -1,4 +1,4 @@
-import { AddDebitModel, DebitModel, AddDebitRepository } from './db-add-debit-protocols'
+import { AddDebitModel, DebitModel, AddDebitRepository, ClientModel,LoadClientByIdRepository } from './db-add-debit-protocols'
 import { DbAddDebit } from './db-add-debit'
 
 const makeFakeDebit = (): DebitModel => ({
@@ -15,7 +15,37 @@ const makeFakeDebitData = (): AddDebitModel => ({
   date: 'any_date',
   value: 'any_value'
 })
-
+const makeFakeLoadClientData = (): ClientModel => ({
+  id: 'any_clientId',
+  name: 'any_name',
+  username: 'any_username',
+  email: 'any_email@email.com',
+  address: {
+    street: 'any_street',
+    suite: 'any_suite',
+    city: 'any_city',
+    zipcode: 'any_zipcode',
+    geo: {
+      lat: 'any_lat' ,
+      lng: 'any_lng'
+    }
+  },
+  phone: 'any_phone',
+  website: 'any_website',
+  company: {
+    name: 'any_companyname',
+    catchPhrase: 'any_catchphrase',
+    bs: 'any_bs'
+  }
+})
+const makeLoadClientByRepository = (): LoadClientByIdRepository => {
+  class LoadClientByIdRepositoryStub implements LoadClientByIdRepository {
+    async loadById (id: string): Promise<ClientModel> {
+      return new Promise(resolve => resolve(makeFakeLoadClientData()))
+    }
+  }
+  return new LoadClientByIdRepositoryStub()
+}
 const makeAddDebitRepository = (): AddDebitRepository => {
   class AddDebitRepositoryStub implements AddDebitRepository {
     async add (accountData: AddDebitModel): Promise<DebitModel> {
@@ -28,23 +58,26 @@ const makeAddDebitRepository = (): AddDebitRepository => {
 interface SutTypes {
   sut: DbAddDebit
   addDebitRepositoryStub: AddDebitRepository
+  loadClientByIdRepositoryStub: LoadClientByIdRepository
 }
 
 const makeSut = (): SutTypes => {
+  const loadClientByIdRepositoryStub = makeLoadClientByRepository()
   const addDebitRepositoryStub = makeAddDebitRepository()
-  const sut = new DbAddDebit(addDebitRepositoryStub)
+  const sut = new DbAddDebit(addDebitRepositoryStub,loadClientByIdRepositoryStub)
   return {
     sut,
-    addDebitRepositoryStub
+    addDebitRepositoryStub,
+    loadClientByIdRepositoryStub
   }
 }
 
 describe('DbAddDebit usecase', () => {
   test('Should call AddDebitRepository with correct values', async () => {
     const { sut, addDebitRepositoryStub } = makeSut()
-    const loadSpy = jest.spyOn(addDebitRepositoryStub,'add')
+    const addSpy = jest.spyOn(addDebitRepositoryStub,'add')
     await sut.add(makeFakeDebitData())
-    expect(loadSpy).toHaveBeenCalledWith(makeFakeDebitData())
+    expect(addSpy).toHaveBeenCalledWith(makeFakeDebitData())
   })
 
   test('Should throws if AddDebitRepository throws', async () => {
@@ -52,5 +85,12 @@ describe('DbAddDebit usecase', () => {
     jest.spyOn(addDebitRepositoryStub,'add').mockReturnValueOnce(new Promise((resolve,reject) => reject(new Error())))
     const promise = sut.add(makeFakeDebitData())
     await expect(promise).rejects.toThrow()
+  })
+
+  test('Should call  LoadClientByIdRepository with correct values', async () => {
+    const { sut,loadClientByIdRepositoryStub } = makeSut()
+    const loadSpy = jest.spyOn(loadClientByIdRepositoryStub,'loadById')
+    await sut.add(makeFakeDebitData())
+    expect(loadSpy).toBeCalledWith('any_clientId')
   })
 })
