cb2aa4ee3b73a1bd9f5dd90b7cc26fdce417749b
Caio Vieira <caiodmvieiragithub@gmail.com>
2020-10-17 17:21:39 -0300

    refactor: move sut creation to a factory helper method

--- src/data/usecases/add-debit/db-add-debit.spec.ts
+++ src/data/usecases/add-debit/db-add-debit.spec.ts
@@ -1,33 +1,49 @@
 import { AddDebitModel, DebitModel, AddDebitRepository } from './db-add-debit-protocols'
 import { DbAddDebit } from './db-add-debit'
 
+const makeFakeDebit = (): DebitModel => ({
+  id: 'any_id',
+  clientId: 'any_clientId',
+  reason: 'any_reason',
+  date: 'any_date',
+  value: 'any_value'
+})
+
+const makeFakeDebitData = (): AddDebitModel => ({
+  clientId: 'any_clientId',
+  reason: 'any_reason',
+  date: 'any_date',
+  value: 'any_value'
+})
+
+const makeAddDebitRepository = (): AddDebitRepository => {
+  class AddDebitRepositoryStub implements AddDebitRepository {
+    async add (accountData: AddDebitModel): Promise<DebitModel> {
+      return new Promise(resolve => resolve(makeFakeDebit()))
+    }
+  }
+  return new AddDebitRepositoryStub()
+}
+
+interface SutTypes {
+  sut: DbAddDebit
+  addDebitRepositoryStub: AddDebitRepository
+}
+
+const makeSut = (): SutTypes => {
+  const addDebitRepositoryStub = makeAddDebitRepository()
+  const sut = new DbAddDebit(addDebitRepositoryStub)
+  return {
+    sut,
+    addDebitRepositoryStub
+  }
+}
+
 describe('DbAddDebit usecase', () => {
   test('Should call AddDebitRepository with correct values', async () => {
-    class AddDebitRepositoryStub implements AddDebitRepository {
-      async add (accountData: AddDebitModel): Promise<DebitModel> {
-        return new Promise(resolve => resolve({
-          id: 'any_id',
-          clientId: 'any_clientId',
-          reason: 'any_reason',
-          date: 'any_date',
-          value: 'any_value'
-        }))
-      }
-    }
-    const addDebitRepositoryStub = new AddDebitRepositoryStub()
-    const sut = new DbAddDebit(addDebitRepositoryStub)
+    const { sut, addDebitRepositoryStub } = makeSut()
     const loadSpy = jest.spyOn(addDebitRepositoryStub,'add')
-    await sut.add({
-      clientId: 'any_clientId',
-      reason: 'any_reason',
-      date: 'any_date',
-      value: 'any_value'
-    })
-    expect(loadSpy).toHaveBeenCalledWith({
-      clientId: 'any_clientId',
-      reason: 'any_reason',
-      date: 'any_date',
-      value: 'any_value'
-    })
+    await sut.add(makeFakeDebitData())
+    expect(loadSpy).toHaveBeenCalledWith(makeFakeDebitData())
   })
 })
