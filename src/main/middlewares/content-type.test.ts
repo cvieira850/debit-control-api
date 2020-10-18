d7293409ab74e13d679e9e581b8b59130c1e5375
Caio Vieira <caiodmvieiragithub@gmail.com>
2020-10-18 11:27:01 -0300

    test: ensure requests return json as default content type

---
+++ src/main/middlewares/content-type.test.ts
@@ -0,0 +1,13 @@
+import app from '../config/app'
+import request from 'supertest'
+
+describe('Content Type Middleware', () => {
+  test('Should return default content type as json', async () => {
+    app.get('/test_content_type', (req, res) => {
+      res.send()
+    })
+    await request(app)
+      .get('/test_content_type')
+      .expect('content-type',/json/)
+  })
+})
