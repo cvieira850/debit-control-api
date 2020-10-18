ca293ebd782efda981c8f94964a1fdfe0c40e1cb
Caio Vieira <caiodmvieiragithub@gmail.com>
2020-10-18 19:18:08 -0300

    refactor: move sut creation to a factory helper method

--- src/presentation/controllers/debits/load-debit-by-id/load-debit-by-id-controller.spec.ts
+++ src/presentation/controllers/debits/load-debit-by-id/load-debit-by-id-controller.spec.ts
@@ -1,27 +1,44 @@
 import { LoadDebitByIdController } from './load-debit-by-id-controller'
-import { DebitModel, LoadDebitById } from './load-debit-by-id-protocols'
+import { DebitModel, LoadDebitById, HttpRequest } from './load-debit-by-id-protocols'
 
+const makeFakeRequest = (): HttpRequest => ({
+  params: {
+    debitId: 'any_debit_id'
+  }
+})
+const makeFakeDebit = (): DebitModel => ({
+  id: 'any_debit_id',
+  clientId: 'any_clientId',
+  reason: 'any_reason',
+  date: 'any_date',
+  value: 'any_value'
+})
+const makeLoadDebitById = (): LoadDebitById => {
+  class LoadDebitByIdStub implements LoadDebitById {
+    async load (id: string): Promise<DebitModel> {
+      return new Promise(resolve => resolve(makeFakeDebit()))
+    }
+  }
+  return new LoadDebitByIdStub()
+}
+interface SutTypes {
+  sut: LoadDebitByIdController
+  loadDebitByIdStub: LoadDebitById
+}
+
+const makeSut = (): SutTypes => {
+  const loadDebitByIdStub = makeLoadDebitById()
+  const sut = new LoadDebitByIdController(loadDebitByIdStub)
+  return {
+    sut,
+    loadDebitByIdStub
+  }
+}
 describe('LoadDebitById Controller', () => {
   test('Should call LoadDebitById with corrrect id', async () => {
-    class LoadDebitByIdStub implements LoadDebitById {
-      async load (id: string): Promise<DebitModel> {
-        return new Promise(resolve => resolve({
-          id: 'any_debit_id',
-          clientId: 'any_clientId',
-          reason: 'any_reason',
-          date: 'any_date',
-          value: 'any_value'
-        }))
-      }
-    }
-    const loadDebitByIdStub = new LoadDebitByIdStub()
-    const sut = new LoadDebitByIdController(loadDebitByIdStub)
+    const { sut,loadDebitByIdStub } = makeSut()
     const loadByIdSpy = jest.spyOn(loadDebitByIdStub,'load')
-    await sut.handle({
-      params: {
-        debitId: 'any_debit_id'
-      }
-    })
+    await sut.handle(makeFakeRequest())
     expect(loadByIdSpy).toHaveBeenCalledWith('any_debit_id')
   })
 })
