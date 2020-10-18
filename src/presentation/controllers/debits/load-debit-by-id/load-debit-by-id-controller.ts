79b9dc85f252605e56449b1970e57f3b958e2d30
Caio Vieira <caiodmvieiragithub@gmail.com>
2020-10-18 19:26:24 -0300

    feat: ensure LoadDebitByIdController returns 200 if valid data is provided

--- src/presentation/controllers/debits/load-debit-by-id/load-debit-by-id-controller.ts
+++ src/presentation/controllers/debits/load-debit-by-id/load-debit-by-id-controller.ts
@@ -1,12 +1,12 @@
 import { Controller,HttpRequest, HttpResponse, LoadDebitById } from './load-debit-by-id-protocols'
-import { serverError } from '../../../helpers/http/http-helpers'
+import { serverError, ok } from '../../../helpers/http/http-helpers'
 
 export class LoadDebitByIdController implements Controller {
   constructor (private readonly loadDebitById: LoadDebitById) {}
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      await this.loadDebitById.load(httpRequest.params.debitId)
-      return null
+      const debit = await this.loadDebitById.load(httpRequest.params.debitId)
+      return ok(debit)
     } catch (error) {
       return serverError(error)
     }
