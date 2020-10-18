57efd3905d949c3a55c5f10d772d49426908f30b
Caio Vieira <caiodmvieiragithub@gmail.com>
2020-10-18 19:22:32 -0300

    feat: ensure LoadDebitByIdController throw if LoadDebitById throws

--- src/presentation/controllers/debits/load-debit-by-id/load-debit-by-id-controller.ts
+++ src/presentation/controllers/debits/load-debit-by-id/load-debit-by-id-controller.ts
@@ -1,9 +1,14 @@
 import { Controller,HttpRequest, HttpResponse, LoadDebitById } from './load-debit-by-id-protocols'
+import { serverError } from '../../../helpers/http/http-helpers'
 
 export class LoadDebitByIdController implements Controller {
   constructor (private readonly loadDebitById: LoadDebitById) {}
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.loadDebitById.load(httpRequest.params.debitId)
-    return new Promise(resolve => resolve(null))
+    try {
+      await this.loadDebitById.load(httpRequest.params.debitId)
+      return null
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
