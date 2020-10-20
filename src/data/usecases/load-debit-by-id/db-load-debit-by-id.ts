b47599279ab551e6f3f6fdb55238f1acb9f5c344
Caio Vieira <caiodmvieiragithub@gmail.com>
2020-10-20 20:02:13 -0300

    feat: ensure DbLoadDebitById return a debit on success

--- src/data/usecases/load-debit-by-id/db-load-debit-by-id.ts
+++ src/data/usecases/load-debit-by-id/db-load-debit-by-id.ts
@@ -4,7 +4,7 @@ import { DebitModel } from './db-load-debit-by-id-protocols'
 export class DbLoadDebitById implements LoadDebitByIdRepository {
   constructor (private readonly loadDebitByIdRepository: LoadDebitByIdRepository) {}
   async loadById (id: string): Promise<DebitModel> {
-    await this.loadDebitByIdRepository.loadById(id)
-    return new Promise(resolve => resolve(null))
+    const debit = await this.loadDebitByIdRepository.loadById(id)
+    return debit
   }
 }
