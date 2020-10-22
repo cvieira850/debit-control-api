7300fb872a5cda9e07c73dd83b2e17708a8732e9
Caio Vieira <caiodmvieiragithub@gmail.com>
2020-10-21 22:14:08 -0300

    feat: ensure LoadDebitsController return 204 if LoadDebits returns an empty array

--- src/presentation/controllers/debits/load-debits/load-debits-controller.ts
+++ src/presentation/controllers/debits/load-debits/load-debits-controller.ts
@@ -1,12 +1,14 @@
 import { Controller, HttpRequest, HttpResponse,LoadDebits } from './load-debits-protocols'
-import { ok,serverError } from '../../../helpers/http/http-helpers'
+import { ok,serverError, noContent } from '../../../helpers/http/http-helpers'
 export class LoadDebitsController implements Controller {
   constructor (private readonly loadDebits: LoadDebits) {}
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const debits = await this.loadDebits.load()
-      console.log()
-      return ok(debits)
+      if (debits.length > 0) {
+        return ok(debits)
+      }
+      return noContent()
     } catch (error) {
       return serverError(error)
     }
