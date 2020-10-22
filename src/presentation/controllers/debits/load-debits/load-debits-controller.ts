2c7768bc7d0b14f07d9c8e7d3ec449658dbe037e
Caio Vieira <caiodmvieiragithub@gmail.com>
2020-10-21 22:06:40 -0300

    feat: ensure LoadDebitsController throw if LoadDebits throws

--- src/presentation/controllers/debits/load-debits/load-debits-controller.ts
+++ src/presentation/controllers/debits/load-debits/load-debits-controller.ts
@@ -1,11 +1,14 @@
 import { Controller, HttpRequest, HttpResponse,LoadDebits } from './load-debits-protocols'
-import { ok } from '../../../helpers/http/http-helpers'
-
+import { ok,serverError } from '../../../helpers/http/http-helpers'
 export class LoadDebitsController implements Controller {
   constructor (private readonly loadDebits: LoadDebits) {}
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const debits = await this.loadDebits.load()
-    console.log()
-    return ok(debits)
+    try {
+      const debits = await this.loadDebits.load()
+      console.log()
+      return ok(debits)
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
