fa81d5e031b8ec3abf15537002068729bf2ed31f
Caio Vieira <caiodmvieiragithub@gmail.com>
2020-10-22 20:27:38 -0300

    refactor: move sut creation to a factory helper method

--- src/data/usecases/load-debits/db-load-debits.spec.ts
+++ src/data/usecases/load-debits/db-load-debits.spec.ts
@@ -2,15 +2,30 @@ import { DebitModel } from './db-load-debits-protocols'
 import { LoadDebitsRepository } from '../../protocols/debit/load-debits-repository'
 import { DbLoadDebits } from './db-load-debits'
 
+const makeLoadDebits = (): LoadDebitsRepository => {
+  class LoadDebitsRepositoryStub implements LoadDebitsRepository {
+    async load (): Promise<DebitModel[]> {
+      return new Promise(resolve => resolve([]))
+    }
+  }
+  return new LoadDebitsRepositoryStub()
+}
+interface SutTypes {
+  sut: DbLoadDebits
+  loadDebitsRepositoryStub: LoadDebitsRepository
+}
+
+const makeSut = (): SutTypes => {
+  const loadDebitsRepositoryStub = makeLoadDebits()
+  const sut = new DbLoadDebits(loadDebitsRepositoryStub)
+  return {
+    sut,
+    loadDebitsRepositoryStub
+  }
+}
 describe('DbLoadDebits Usecase', () => {
   test('Should call LoadDebitsRepository with no value', async () => {
-    class LoadDebitsRepositoryStub implements LoadDebitsRepository {
-      async load (): Promise<DebitModel[]> {
-        return new Promise(resolve => resolve([]))
-      }
-    }
-    const loadDebitsRepositoryStub = new LoadDebitsRepositoryStub()
-    const sut = new DbLoadDebits(loadDebitsRepositoryStub)
+    const { sut, loadDebitsRepositoryStub } = makeSut()
     const loadSpy = jest.spyOn(loadDebitsRepositoryStub,'load')
     await sut.load()
     expect(loadSpy).toHaveBeenCalledWith()
