f5eac941207a637eace641d432a14e82cbba4e6b
Caio Vieira <caiodmvieiragithub@gmail.com>
2020-10-22 20:30:20 -0300

    feat: ensure DbLoadDebits returns an array of debits

--- src/data/usecases/load-debits/db-load-debits.ts
+++ src/data/usecases/load-debits/db-load-debits.ts
@@ -4,7 +4,7 @@ import { DebitModel } from './db-load-debits-protocols'
 export class DbLoadDebits implements LoadDebitsRepository {
   constructor (private readonly loadDebitsRepository: LoadDebitsRepository) {}
   async load (): Promise<DebitModel[]> {
-    await this.loadDebitsRepository.load()
-    return new Promise(resolve => resolve(null))
+    const debits = await this.loadDebitsRepository.load()
+    return new Promise(resolve => resolve(debits))
   }
 }
