b916ad32878fc75375c2a219bc30f185255a9061
Caio Vieira <caiodmvieiragithub@gmail.com>
2020-10-22 21:43:12 -0300

    refactor: move sut creation to a factory helper method

--- src/presentation/controllers/debits/delete-debit/delete-debit-controller.spec.ts
+++ src/presentation/controllers/debits/delete-debit/delete-debit-controller.spec.ts
@@ -1,16 +1,38 @@
-import { DeleteDebit } from './delete-debit-protocols'
+import { DeleteDebit, HttpRequest } from './delete-debit-protocols'
 import { DeleteDebitController } from './delete-debit-controller'
+
+const makeRequest = (): HttpRequest => ({
+  params: { id: 1 }
+})
+
+const makeDeleteDebit = (): DeleteDebit => {
+  class DeleteDebitStub implements DeleteDebit {
+    async delete (id: string): Promise<void> {
+      return new Promise(resolve => resolve())
+    }
+  }
+  return new DeleteDebitStub()
+}
+
+interface SutTypes {
+  sut: DeleteDebitController
+  deleteDebitStub: DeleteDebit
+}
+
+const makeSut = (): SutTypes => {
+  const deleteDebitStub = makeDeleteDebit()
+  const sut = new DeleteDebitController(deleteDebitStub)
+  return {
+    sut,
+    deleteDebitStub
+  }
+}
+
 describe('DeleteDebit Controller', () => {
   test('Should call DeleteDebit with correct id', async () => {
-    class DeleteDebitStub implements DeleteDebit {
-      async delete (id: string): Promise<void> {
-        return new Promise(resolve => resolve())
-      }
-    }
-    const deleteDebitStub = new DeleteDebitStub()
-    const sut = new DeleteDebitController(deleteDebitStub)
+    const { sut, deleteDebitStub } = makeSut()
     const deleteSpy = jest.spyOn(deleteDebitStub,'delete')
-    await sut.handle({ params: { id: 1 } })
+    await sut.handle(makeRequest())
     expect(deleteSpy).toHaveBeenCalledWith(1)
   })
 })
