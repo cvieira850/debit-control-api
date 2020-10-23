c137324737c31a0269973171d2138a5672bb6f4e
Caio Vieira <caiodmvieiragithub@gmail.com>
2020-10-22 21:49:06 -0300

    feat: ensure DeleteDebitController throw if DeleteDebit throws

--- src/presentation/controllers/debits/delete-debit/delete-debit-controller.ts
+++ src/presentation/controllers/debits/delete-debit/delete-debit-controller.ts
@@ -1,10 +1,15 @@
 import { Controller,HttpRequest,HttpResponse, DeleteDebit } from './delete-debit-protocols'
+import { serverError } from '../../../helpers/http/http-helpers'
 
 export class DeleteDebitController implements Controller {
   constructor (private readonly deleteDebit: DeleteDebit) {}
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { id } = httpRequest.params
-    await this.deleteDebit.delete(id)
-    return null
+    try {
+      const { id } = httpRequest.params
+      await this.deleteDebit.delete(id)
+      return null
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
