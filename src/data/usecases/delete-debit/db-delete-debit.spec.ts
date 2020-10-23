1101d69afebb0cdde8e8c3ab8355a616c941a2ad
Caio Vieira <caiodmvieiragithub@gmail.com>
2020-10-22 22:07:35 -0300

    refactor: move sut creation to a factory helper method

--- src/data/usecases/delete-debit/db-delete-debit.spec.ts
+++ src/data/usecases/delete-debit/db-delete-debit.spec.ts
@@ -1,15 +1,30 @@
 import { DeleteDebitRepository } from './db-delete-debit-protocols'
 import { DbDeleteDebit } from './db-delete-debit'
 
+const makeDeleteDebitRepository = (): DeleteDebitRepository => {
+  class DeleteDebitRepositoryStub implements DeleteDebitRepository {
+    async delete (id: number): Promise<void> {
+      return new Promise(resolve => resolve(null))
+    }
+  }
+  return new DeleteDebitRepositoryStub()
+}
+
+interface SutTypes {
+  sut: DbDeleteDebit
+  deleteDebitRepositoryStub: DeleteDebitRepository
+}
+const makeSut = (): SutTypes => {
+  const deleteDebitRepositoryStub = makeDeleteDebitRepository()
+  const sut = new DbDeleteDebit(deleteDebitRepositoryStub)
+  return {
+    sut,
+    deleteDebitRepositoryStub
+  }
+}
 describe('DbDeleteDebit usecase', () => {
   test('Should call DeleteDebitRepositoy with correct id', async () => {
-    class DeleteDebitRepositoryStub implements DeleteDebitRepository {
-      async delete (id: number): Promise<void> {
-        return new Promise(resolve => resolve(null))
-      }
-    }
-    const deleteDebitRepositoryStub = new DeleteDebitRepositoryStub()
-    const sut = new DbDeleteDebit(deleteDebitRepositoryStub)
+    const { sut,deleteDebitRepositoryStub } = makeSut()
     const deleteSpy = jest.spyOn(deleteDebitRepositoryStub,'delete')
     await sut.delete(1)
     expect(deleteSpy).toHaveBeenCalledWith(1)
